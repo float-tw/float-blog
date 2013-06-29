17578d2f97d801b3d126d18fbe9fd239ea852829
float <a13524000@gmail.com>
2013-06-29 22:57:23 +0800

    change rst2html version

--- gen_html.sh
+++ gen_html.sh
@@ -4,4 +4,4 @@ if [ $# != 1 ]; then
 	echo 'no input file or too many';
 fi
 
-rst2html --link-stylesheet $1 > $(echo $1 | cut -d '.' -f 1).html
+rst2html.py --link-stylesheet $1 > $(echo $1 | cut -d '.' -f 1).html
