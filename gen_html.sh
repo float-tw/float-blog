e1359aafbf0376e35fafe14d1c8ad71fc7681847
float <a13524000@gmail.com>
2013-06-29 23:50:32 +0800

    put css in html file

--- gen_html.sh
+++ gen_html.sh
@@ -4,4 +4,5 @@ if [ $# != 1 ]; then
 	echo 'no input file or too many';
 fi
 
-rst2html.py --link-stylesheet $1 > $(echo $1 | cut -d '.' -f 1).html
+rst2html.py $1 > $(echo $1 | cut -d '.' -f 1).html
+#rst2html.py --link-stylesheet $1 > $(echo $1 | cut -d '.' -f 1).html
