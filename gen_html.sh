1f80217612f6634228fa6323baac9a72d31c0e70
float <a13524000@gmail.com>
2013-07-02 17:20:21 +0800

    using new style css

--- gen_html.sh
+++ gen_html.sh
@@ -4,5 +4,6 @@ if [ $# != 1 ]; then
 	echo 'no input file or too many';
 fi
 
-rst2html.py $1 > $(echo $1 | cut -d '.' -f 1).html
+rst2html.py --stylesheet=style.css --syntax-highlight=short $1 > $(echo $1 | cut -d '.' -f 1).html
+#rst2html.py --stylesheet=style.css --syntax-highlight=short test.rst > test.html
 #rst2html.py --link-stylesheet $1 > $(echo $1 | cut -d '.' -f 1).html
